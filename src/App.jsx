91385410a0bd9ab4640c6efd5e3b092c0668e517
TutuIsPlayer <98626561+TutuIsPlayer@users.noreply.github.com>
2023-12-14 14:39:17 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AppRoutes from './routes.jsx'
+
+function App() {
+  return (
+    <>
+ 
+ <AppRoutes />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Sobre/index.jsx
@@ -0,0 +1,46 @@
+import styles from './Sobre.module.css'
+import avatar from './images/avatar.svg'
+import html from './images/icon-html.svg'
+import css from './images/icon-css.svg'
+import js from './images/icon-js.svg'
+import react from './images/icon-react.svg'
+import node from './images/icon-node.svg'
+import sql from './images/icon-sql.svg'
+
+function Sobre() {
+    return (
+        <section className={styles.sobre}>
+            
+            <div className={styles.bio}>
+                <img src={avatar} alt="Avatar" className={styles.avatar} />
+                <div className={styles.textos}>
+                    <h2>Sobre</h2>
+
+                    <p>Meu nome é <span>Arthur Freitas.</span> <br />
+                    <strong>Estudante</strong> </p>
+
+                    <p>Trabalho com desenvolvimento Web desde 2022.</p>
+
+                    <p>Adorotransformar ideias em realidade digital.</p>
+
+                    <p>Fico muito orgulhoso em testemunhar a minha evolução pessoal e profissional</p>
+                </div>
+            </div>
+
+            <div className={styles.techs}>
+                <h3>Linguagens</h3>
+                <div className={styles.icones}>
+                    <img src={html} alt="Ícone do html" />
+                    <img src={css} alt="Ícone do css" />
+                    <img src={js} alt="Ícone do js" />
+                    <img src={react} alt="Ícone do react" />
+                    <img src={node} alt="Ícone do node" />
+                    <img src={sql} alt="Ícone do sql" />
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Sobre
